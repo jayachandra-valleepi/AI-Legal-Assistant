6ca818b7b79f18f068abaf2f51c8b7c5f8de7ba6
Jayachadnra-v77 <jayachandra.v986@gmail.com>
2026-05-23 13:05:31 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "AI_Legal_Aissitant",
+    version="0.1.0",
+    author="Vallepu Jayachandra",
+    author_email= "jayachandra.v986@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
--- template.sh
+++ template.sh
@@ -8,7 +8,7 @@ mkdir -p research
 
 # Creating files
 
-touch src/__init__.py
+touch src/__init__.py 
 touch src/helper.py
 touch src/prompt.py
 touch .env
